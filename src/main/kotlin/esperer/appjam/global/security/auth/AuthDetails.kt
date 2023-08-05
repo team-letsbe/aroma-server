dd9978b5f7bc1bb713c713bd0c194840114d60f0
aesperer <piyrw9754@gmail.com>
2023-08-05 17:54:32 +0900

    user auth details; gp origin master
    ;

--- src/main/kotlin/esperer/appjam/global/security/auth/AuthDetails.kt
+++ src/main/kotlin/esperer/appjam/global/security/auth/AuthDetails.kt
@@ -8,31 +8,24 @@ class AuthDetails(
     private val user: User
 ) : UserDetails {
 
-    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
-        TODO("Not yet implemented")
-    }
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = mutableListOf(user.role)
 
-    override fun getPassword(): String {
-        TODO("Not yet implemented")
-    }
+    override fun getPassword(): String? =
+        null
 
-    override fun getUsername(): String {
-        TODO("Not yet implemented")
-    }
+    override fun getUsername(): String =
+        user.id.toString()
 
-    override fun isAccountNonExpired(): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun isAccountNonExpired(): Boolean =
+        true
 
-    override fun isAccountNonLocked(): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun isAccountNonLocked(): Boolean =
+        true
 
-    override fun isCredentialsNonExpired(): Boolean {
-        TODO("Not yet implemented")
-    }
 
-    override fun isEnabled(): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun isCredentialsNonExpired(): Boolean =
+        true
+
+    override fun isEnabled(): Boolean =
+        isAccountNonExpired && isAccountNonLocked && isCredentialsNonExpired
 }
